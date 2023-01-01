bedc3425d7cb0f3806359b41b7b3d30f30e7105b
Nishal Barman <nishalbarman@gmail.com>
2023-01-02 00:45:34 +0530

    Updates files

--- core/base.php
+++ core/base.php
@@ -1,7 +1,7 @@
 <?php
 
-// $BASE_URL = "http://13.127.248.80";
-$BASE_URL = "http://localhost/hk_new";
+$BASE_URL = "http://13.127.248.80";
+// $BASE_URL = "http://localhost/hk_new";
 $ENGINE_PATH = "/core/engine/main_engine.php";
 
 ?>
